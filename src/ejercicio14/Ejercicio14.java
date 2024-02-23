16ec2dea3e0d08dd9eaea7948c5d9cfdb3f40191
juancoficial7 <jherrerasi@unal.edu.co>
2024-02-22 20:26:15 -0500

    first commit

---
+++ src/ejercicio14/Ejercicio14.java
@@ -0,0 +1,18 @@
+
+package ejercicio14;
+
+
+public class Ejercicio14 {
+
+    
+    public static void main(String[] args) {
+        double numero, cuadrado, cubo;
+        numero = 2;
+        cuadrado = Math.pow(numero, 2);
+        cubo = Math.pow(numero, 3);
+        System.out.println("El numero es: "+ numero);
+        System.out.println("El cuadrado es: "+ cuadrado);
+        System.out.println("El cubo es: "+ cubo);
+    }
+    
+}
